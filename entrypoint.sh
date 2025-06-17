5574e45536f48e948cbc55cadd824116d0258c87
Sven R <44927507+cr0ws7@users.noreply.github.com>
2025-06-17 22:03:03 +0200

    allow checkpoints folder and use int8

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,15 @@
 CUDA_ENABLED=${CUDA_ENABLED:-true}
 DEVICE=""
 
+LLAMA_CHECKPOINT_PATH="checkpoints/openaudio-s1-mini-int8"
+DECODER_CHECKPOINT_PATH="checkpoints/openaudio-s1-mini-int8/codec.pth"
+
 if [ "${CUDA_ENABLED}" != "true" ]; then
     DEVICE="--device cpu"
 fi
 
-exec "$(command -v python3 || command -v python)" tools/run_webui.py ${DEVICE}
+exec "$(command -v python3 || command -v python)" tools/run_webui.py \
+    --llama-checkpoint-path "${LLAMA_CHECKPOINT_PATH}" \
+    --decoder-checkpoint-path "${DECODER_CHECKPOINT_PATH}" \
+    --compile \
+    ${DEVICE}
