14f120c46646188a41969bdde31eb2e4ab1f72e8
Sven R <44927507+cr0ws7@users.noreply.github.com>
2025-06-21 19:27:02 +0200

    use half to use 16bit. should speed up

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,13 @@
 
 CUDA_ENABLED=${CUDA_ENABLED:-true}
 DEVICE=""
+HALF=""
 
 if [ "${CUDA_ENABLED}" != "true" ]; then
     DEVICE="--device cpu"
+else
+    HALF="--half"
 fi
 
 exec "$(command -v python3 || command -v python)" tools/run_webui.py \
-    ${DEVICE}
+    ${DEVICE} ${HALF}
